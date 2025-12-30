052b43df98649c60ca0116f0f387b9fcf9dfebd6
SanjeetaAcharya <sanjitashanti@gmail.com>
2025-12-30 14:11:58 +0545

    New updates

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,4 @@
-const String serverIP = "192.168.1.9";
+const String serverIP = "192.168.1.10";
 const int serverPort = 9000;
 const String serverProtocol = "http";
-
 String get baseUrl => '$serverProtocol://$serverIP:$serverPort';
